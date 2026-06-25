17c914573f63ffc22dd127e8f50d2249d5aa4d80
Malli Nandini <mallinandini9@gmail.com>
2026-06-25 11:46:10 +0530

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(ClientRegister_Model)
+admin.site.register(hybrid_feature_model)
+admin.site.register(review_Model)
+admin.site.register(recommend_Model)
+admin.site.register(tweet_accuracy_model)
